399554cabe84d99b46851d5955b2646702896088
Giga Chang <gigayaya@gmail.com>
2018-05-31 22:05:58 +0800

    fix

--- keras_model.py
+++ keras_model.py
@@ -2,6 +2,15 @@ from keras.models import *
 from keras.layers import *
 from keras.optimizers import *
 
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+
+config = tf.ConfigProto()
+#config.gpu_options.per_process_gpu_memory_fraction = 0.1
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
+
 def get_model():
     model = Sequential()
     model.add(Dense(units=10, input_dim=1,activation='relu'))
--- main.py
+++ main.py
@@ -2,29 +2,36 @@ import multiprocessing
 from keras_model import get_model
 import os
 import tensorflow as tf
+import traceback
+from keras.backend.tensorflow_backend import get_session,clear_session,set_session
 
-def do_job(model,iter_num):
+
+def do_job(iter_num):
+    model = get_model()
     x = [12345,54321,00000,1111]
     y = [1,1,0,0]
     model.fit(x=x, y=y,validation_split=0.5,epochs=10,shuffle=True,batch_size=2,verbose=2)
     print("Done process "+str(iter_num))
+    clear_session()
+    print("close")
 
 def func(iter_num):
-    print("Start process "+str(iter_num))
-    if(iter_num%2==0):
-        os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-        with tf.device('/gpu:0'):
-            model = get_model()
-            do_job(model,iter_num)
-    else:
-        os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-        with tf.device('/gpu:1'):
-            model = get_model()
-            do_job(model,iter_num)
+    try:
+        print("Start process "+str(iter_num))
+        if(iter_num%2==0):
+            os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+            #with tf.device('/gpu:0'):
+            do_job(iter_num)
+        else:
+            os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+            #with tf.device('/gpu:1'):
+            do_job(iter_num)
+    except Exception:
+        print(traceback.format_exc())
 
 if __name__ == "__main__":
-    pool = multiprocessing.Pool(processes=2)
-    for i in range(40):
+    pool = multiprocessing.Pool(processes=4)
+    for i in range(8):
         pool.apply_async(func,args=(i,))
     pool.close()
     pool.join()
